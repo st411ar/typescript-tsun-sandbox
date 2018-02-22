d370179a41e84c87578f37d27320e68b546db579
Vitaly Kiselev <st411ar@ya.ru>
2018-02-22 16:08:54 +0300

    added report example which contains inheritance, fat arrow functions, multiarrays

--- inheritance-sandbox.ts
+++ inheritance-sandbox.ts
@@ -34,3 +34,66 @@ var headers: string[] = ['Name'];
 var data: string[] = ['Alice Green', 'Paul Pfifer', 'Louis Blakenship'];
 var tr: TabbedReport = new TabbedReport(headers, data);
 tr.run();
+
+
+function pad(s: any, len: number, pad: string): string {
+	s = s + '';
+	var spaces = (len + 1) - s.length;
+	return s + Array(spaces).join(pad);
+}
+
+console.log(pad('sample string', 15, '<>'));
+
+
+class FormattedReport extends Report {
+	headers: string[];
+	values: any[][];
+	
+	constructor(headers: string[], values: any[][]) {
+		var lengths: number[] = headers.map((header: string, i: number) => {
+			var maxLength: number = header.length;
+			values.forEach((value: any[]) => {
+				var valueLength: number = (value[i] + '').length;
+				if (valueLength > maxLength) {
+					maxLength = valueLength;
+				}
+			});
+			return maxLength;
+		});
+		
+		var data: Array<string> = [];
+		
+		data.push(
+			headers
+				.map((h, i) => pad(h, lengths[i], ' '))
+				.join(" ")
+		);
+		
+		data.push(
+			headers
+				.map((h, i) => pad('', lengths[i], '-'))
+				.join(" ")
+		);
+		
+		values.map(row => {
+			data.push(
+				row
+					.map((v, i) => pad(v, lengths[i], ' '))
+					.join(" ")
+			);
+		});
+		
+		super(data);
+	}
+}
+
+
+var headers: string[] = ['Name', 'Gender', 'Age'];
+var values = [
+  ['Alice Green', 'Female', 55],
+  ['Paul Pfifer', 'Male', 26],
+  ['Louis Blakenship', 'Male', 44]
+];
+
+var fr: FormattedReport = new FormattedReport(headers, values);
+fr.run();
